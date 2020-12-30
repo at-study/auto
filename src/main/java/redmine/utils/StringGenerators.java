5ea6dbe8834f396ad560510008e9aeeea1819c9e
fan-mag <fan-mag@mail.ru>
2020-12-30 20:25:21 +0300

    Добавлен пример генерации хэша пароля

--- src/main/java/redmine/utils/StringGenerators.java
+++ src/main/java/redmine/utils/StringGenerators.java
@@ -23,7 +23,7 @@ public class StringGenerators {
     public static String randomString(int length, String pattern) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            sb.append(pattern.charAt(new Random().nextInt(length)));
+            sb.append(pattern.charAt(new Random().nextInt(pattern.length())));
         }
         return sb.toString();
     }
